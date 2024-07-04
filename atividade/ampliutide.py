f0a31135913b2a028c196d556e064c542736f66a
LucasPereira1212 <llucaspereirasilva07@gmail.com>
2024-07-04 14:31:44 -0300

    Add files via upload

---
+++ atividade/ampliutide.py
@@ -0,0 +1,7 @@
+import statistics 
+
+def amplitude(lista):
+    maior = max(lista)
+    menor = min(lista)
+    qual = maior - menor
+    return qual
---
+++ atividade/media.py
@@ -0,0 +1,4 @@
+def media(lista):
+    soma = sum(lista)
+    soma2 = soma / 5
+    return soma2
